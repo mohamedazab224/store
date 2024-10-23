87fc3a6c56303608c482a10921d8ca88a664df9b
mohamedazab224 <159525429+mohamedazab224@users.noreply.github.com>
2024-10-23 03:48:53 +0300

    Add files via upload

---
+++ src_styles_css-src_styles_css-src_styles_css.54b5b94eb5415a37.cs
@@ -0,0 +1,11 @@
+@import url(https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap);
+@import url(https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap);
+svg {
+  display: inline-block;
+}
+:focus {
+  box-shadow: none !important;
+}
+
+
+/*# sourceMappingURL=src_styles_css-src_styles_css-src_styles_css.54b5b94eb5415a37.css.map*/
